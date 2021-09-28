da6f79656106d24b47165f5cdfeb94daadbeda63
Natasa Zecevic <nt.zecevic@gmail.com>
2021-09-28 08:59:08 +0200

    pod landing page

---
+++ app.js
@@ -0,0 +1,32 @@
+const button = document.querySelector(".page__form-btn");
+const input = document.querySelector(".page__form-input");
+const form = document.querySelector(".page__form");
+
+button.addEventListener("click", e => {
+  let message = document.querySelector(".page__form-message");
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  let p = document.createElement("p");
+
+  if (message) {
+    message.remove();
+  }
+
+  if (input.value === "") {
+    e.preventDefault();
+    let text = document.createTextNode("Please, enter your email");
+    p.classList.add("page__form-message");
+    p.appendChild(text);
+    form.append(p);
+    return;
+  }
+
+  if (!input.value.match(validRegex)) {
+    e.preventDefault();
+    let text = document.createTextNode("Ooops! Please, check your email");
+    p.classList.add("page__form-message");
+    p.appendChild(text);
+    form.append(p);
+  }
+});
